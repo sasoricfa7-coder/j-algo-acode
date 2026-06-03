be1dbbe3b97e52b9fd5658f077e79d0d0b4ba710
Sasori <sasoricfa7@gmail.com>
2026-06-03 14:38:47 +0200

    Ajout Jeanne_Algo

--- src/main.js
+++ src/main.js
@@ -1,30 +1,32 @@
 import plugin from "../plugin.json";
+import jAlgo from "./language/j_algo.js";
 
 class JAlgoPlugin {
   async init() {
-    console.log("J_Algo chargé");
-    window.toast?.("J_Algo chargé");
+    console.log("Jeanne_Algo chargé");
+
+    if (window.editorManager?.editorLanguages) {
+      editorManager.editorLanguages["Jeanne_Algo"] = jAlgo;
+      window.toast?.("Jeanne_Algo enregistré");
+    } else {
+      window.toast?.("editorLanguages introuvable");
+    }
   }
 
   async destroy() {
-    console.log("J_Algo déchargé");
+    if (window.editorManager?.editorLanguages) {
+      delete editorManager.editorLanguages["Jeanne_Algo"];
+    }
   }
 }
 
 if (window.acode) {
   const instance = new JAlgoPlugin();
 
-  acode.setPluginInit(
-    plugin.id,
-    async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-      if (!baseUrl.endsWith("/")) {
-        baseUrl += "/";
-      }
-
-      instance.baseUrl = baseUrl;
-      await instance.init($page, cacheFile, cacheFileUrl);
-    }
-  );
+  acode.setPluginInit(plugin.id, async (baseUrl) => {
+    instance.baseUrl = baseUrl;
+    await instance.init();
+  });
 
   acode.setPluginUnmount(plugin.id, () => {
     instance.destroy();
