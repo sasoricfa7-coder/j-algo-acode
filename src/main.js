11f3cedd93a11046b47041ef2a394ccfb2ffb4a7
Sasori <sasoricfa7@gmail.com>
2026-06-03 14:08:58 +0200

    Ajout structure J_Algo

--- src/main.js
+++ src/main.js
@@ -1,26 +1,32 @@
-import plugin from '../plugin.json';
+import plugin from "../plugin.json";
 
-class AcodePlugin {
-
+class JAlgoPlugin {
   async init() {
-    // plugin initialisation 
+    console.log("J_Algo chargé");
+    window.toast?.("J_Algo chargé");
   }
 
   async destroy() {
-    // plugin clean up
+    console.log("J_Algo déchargé");
   }
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl += '/';
+  const instance = new JAlgoPlugin();
+
+  acode.setPluginInit(
+    plugin.id,
+    async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+      if (!baseUrl.endsWith("/")) {
+        baseUrl += "/";
+      }
+
+      instance.baseUrl = baseUrl;
+      await instance.init($page, cacheFile, cacheFileUrl);
     }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, cacheFile, cacheFileUrl);
-  });
+  );
+
   acode.setPluginUnmount(plugin.id, () => {
-    acodePlugin.destroy();
+    instance.destroy();
   });
 }
